eccc353f14e17757d5a6b7a963e689448439800e
guilhermelima18 <guilhermelima18@hotmail.com>
2021-07-16 14:29:46 -0300

    terceira aula finalizada

--- src/components/BoxDevs/index.js
+++ src/components/BoxDevs/index.js
@@ -1,21 +1,46 @@
 /* eslint-disable @next/next/no-html-link-for-pages */
 /* eslint-disable @next/next/no-img-element */
 
-import devs from '../../devs';
+import { useEffect, useState } from 'react';
+
 import styles from './styles.module.css';
 
 export default function BoxDevs() {
+  const [followers, setFollowers] = useState([]);
+
+  useEffect(() => {
+    fetch('https://api.github.com/users/guilhermelima18/followers')
+      .then(response => {
+        if (response.ok) {
+          return response.json();
+        }
+
+        throw new Error("Aconteceu algum erro " + response.status);
+      })
+      .then(usersData => {
+        setFollowers(usersData);
+      })
+      .catch(error => {
+        console.error(error)
+      })
+  }, []);
 
   return (
     <div className={styles.content}>
-      <h3>Meus Devs ({devs.length})</h3>
+      <h3>Meus Seguidores ({followers.length})</h3>
       <div className={styles.devs}>
         {
-          devs.slice(0, 6).map((dev) => {
+          followers.slice(0, 6).map(({ id, avatar_url, login }) => {
             return (
-              <a href={`/users/${dev.name}`} key={dev.name} className={styles.dev}>
-                <img src={dev.avatar} alt="" />
-                <h4>{dev.name}</h4>
+              <a
+                key={id}
+                href={`https://github.com/${login}`}
+                target="_blank"
+                rel="noreferrer"
+                className={styles.dev}
+              >
+                <img src={avatar_url} alt={login} />
+                <h4>{login}</h4>
               </a>
             )
           })
